ed5860460b21dfba1a05f0e361a15c1e5826e7b8
JhairDev05 <jhairdev05@gmail.com>
2025-03-13 21:13:41 -0600

    Quitando comentarios

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import sys
 import os
 
-# Obtiene la ruta del entorno virtual
 venv_path = os.path.join(os.path.dirname(__file__), "venv", "Lib", "site-packages")
 
-# Agrega la ruta de las librerías del entorno virtual a sys.path
 sys.path.insert(0, venv_path)
 
-# Define las variables que estás intentando importar
-sqlite = "sqlite:///database.db"  # O la ruta que necesites para tu base de datos SQLite
-secret_key = "tu_clave_secreta_aqui"  # Reemplaza con una clave secreta segura
+sqlite = "sqlite:///database.db"
+secret_key = "tu_clave_secreta_aqui"  
--- main.py
+++ main.py
@@ -4,21 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from utils.db import db
 from config import sqlite, secret_key
 
-# 🔹 Primero creamos la instancia de Flask
 app = Flask(__name__)
 
-# 🔹 Configuramos la base de datos
 app.secret_key = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = sqlite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# 🔹 Inicializamos la base de datos
 db.init_app(app)
 
-# 🔹 Registramos los Blueprints
 app.register_blueprint(creditos_bp)
 
-# 🔹 Evitamos la importación circular asegurándonos de ejecutar esto solo si es el script principal
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
